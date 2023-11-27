5f09b4dd22db2dac84c6eb7b414ee1ce625712c0
[llorentealberto] <[llorentealberto@hotmal.com]>
2023-11-27 23:31:55 +0100

    terminados ejercicios parte1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {path : 'home' ,component : HomeComponent} ,
+  {path : 'register' , component :RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
